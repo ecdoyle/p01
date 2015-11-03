bb2c505069b86a9fe58e2803105ca1f87af7bdc9
ecdoyle <edoyle@temple.edu>
2015-11-02 21:58:07 -0500

    d2

--- foo.cc
+++ foo.cc
@@ -3,25 +3,22 @@
 int main(int argc, char** argv){
 
   int i;
-  Lamp lmp[999]; //PICONE APPROVED THIS VALUE AT 2:30 PM ON MONDAY, NOVEMBER 2ND, 2015 AT HIS OFFICE WITH RAQUEL, CLUUUUIR AND DANA AS WITNESSES
+  //Lamp lmp[999]; //PICONE APPROVED THIS VALUE AT 2:30 PM ON MONDAY, NOVEMBER 2ND, 2015 AT HIS OFFICE WITH RAQUEL, CLUUUUIR AND DANA AS WITNESSES
   
-  Lamp ldata[999];
   List list;
-  /* 
+  
  //check to see if reading arguments in correctly
-  printf("%d", argc);
-  for(i=0; i<2; i++){
-    cout << argv[i];
- }
-  */
+  printf("%d \n", argc);
+  
+  printf("%s \n", argv[1]);
+  
 
   for(i=0;i<argc; i++){  
                        //loop goes from 0 to less than argc because argc is the number of command line arguments, where the first one is the program name 
-    printf("%d /n", i); 
+    printf("%d \n", i); 
     // lmp[i].set_brand(argv[i+1]); 
                                //set_brand has to access argc[i+1] because if it too started at i=0 then you would be attempting to set the brand to a value that really is the program name
-   
-   //ldata[i]= lmp[i]; 
+    
    //if pointer then use ->
    // list.Append(lmp[i]);
    //need to print everything 
@@ -31,7 +28,7 @@ int main(int argc, char** argv){
     lmp[i].debug(stdout);
     }
   */
-  printf("yay! /n");
+  printf("yay! \n");
   return 0;
 }
 
